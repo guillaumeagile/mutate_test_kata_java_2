9b918b431c47b35a342084f86db2984f9b21aef6
guillaume iter <guillaume.saintetienne@iter.org>
2024-02-27 01:04:29 +0100

    mise à jour de l'exerice maj du POM

--- kata_code_smell/src/main/java/fr/mutation/kata2/Exercice.java
+++ kata_code_smell/src/main/java/fr/mutation/kata2/Exercice.java
@@ -10,17 +10,18 @@ public class Exercice {
     public Map<Integer, String> f1(int a1, List<Integer> a2) {
         HashMap<Integer, String> a7 = new HashMap<Integer, String>();
         int a5 = 0;
-        String a9 = "";
+        StringBuilder a9 = new StringBuilder();
         for (int a3 = 0; a3 < a2.size() && a3 < a1; a3++) {
             int a6 = a2.get(a3);
-            if (a6 >= 0) {
-                a9 = a9 + (a6 + " ");
+            int a10 = a6 - 1;
+            if (a6 >= 0 && (a10) < 20) {
+                a9.append(a6).append(" ");
             } else {
                 a5++;
             }
         }
-        a9 += ("\n");
-        a7.put(a5, a9);
+        a9.append("\n");
+        a7.put(a5, a9.toString());
         return a7;
     }
 }
--- kata_code_smell/src/test/java/fr/mutation/kata2/ExerciceTests.java
+++ kata_code_smell/src/test/java/fr/mutation/kata2/ExerciceTests.java
@@ -1,25 +1,17 @@
 package fr.mutation.kata2;
 
-import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.assertThat;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
-import junitparams.JUnitParamsRunner;
+
 import org.assertj.core.api.Assertions;
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 
 
-@RunWith(JUnitParamsRunner.class)
 public class ExerciceTests {
 
-    @Before
-    public void initialise() {
-    }
+
 
     // !!! pour comprendre comment marche une Map/HashMap en java, lisez ceci:  https://www.data-transitionnumerique.com/java-hashmap/
 
@@ -35,9 +27,9 @@ public class ExerciceTests {
         //dans le 1er element de la Map<Int, String> on trouve la clé qui est une entier qui contient le nombre d'entiers positifs trouvés
         // eton trouve dans la valeur <String> de cette map, la chaine de caractère
 
-        assertThat(firstResult.getKey(), is(0));
 
-        Assertions.assertThat(firstResult.getKey()).isZero();  //cette ligne c'est la même chose que la précédente, choisissez l'écriture qui vous plait le plus
+        Assertions.assertThat(firstResult.getKey()).isZero();
+
 
         //2e verification:  que contient la chaine de caractère?
         Assertions.assertThat(firstResult.getValue()).isEqualTo("\n");
@@ -108,8 +100,13 @@ public class ExerciceTests {
         Assertions.assertThat(firstResult.getValue()).isEqualTo("4 9 8 \n");
     }
 
+    //  EXERCICE 1:  Il reste trop de mutant en vie:  les corriger avec des nouveaux tests
+    //  1 nouveau test pour tuer 1 mutant à la fois
+
+
+
     @Test
-    public void listeAvecTroisPositifsLimiteA2() {  // Il reste, un mutant en vie:  le corriger ici
+    public void listeAvecTroisPositifsLimiteA2() {  //  EXERCICE 2:  Il reste un mutant en vie:  le corriger ici
         //given  / ARRANGE
         final Exercice exercice = new Exercice();
         final List<Integer> listeEntiers = new ArrayList<>();
@@ -121,10 +118,11 @@ public class ExerciceTests {
         Map<Integer, String> actual = exercice.f1(2, listeEntiers);
         //then / ASSERT
         Entry<Integer, String> firstResult = actual.entrySet().iterator().next();
-        Assertions.assertThat(firstResult.getKey()).isZero();
-        Assertions.assertThat(firstResult.getValue()).isEqualTo("1 9 \n");
+
     }
 
-/*   EXERCICE : une fois obtenue un Test Strength de 100%, trouvez quels sont les tests qui ne servent à rien...
+
+
+/*   EXERCICE 3 : une fois obtenue un Test Strength de 100%, trouvez quels sont les tests qui ne servent à rien...
     */
 }
