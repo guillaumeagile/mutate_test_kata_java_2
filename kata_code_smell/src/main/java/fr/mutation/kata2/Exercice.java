a14c5c8232d4a1c2904a69ca4eb19a326d268bc6
guillaume iter <guillaume.saintetienne@iter.org>
2024-03-04 19:09:27 +0100

    apparition mutant VOID CALL

--- kata_code_smell/src/main/java/fr/mutation/kata2/Exercice.java
+++ kata_code_smell/src/main/java/fr/mutation/kata2/Exercice.java
@@ -1,8 +1,6 @@
 package fr.mutation.kata2;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 
 public class Exercice {
@@ -14,14 +12,21 @@ public class Exercice {
         for (int a3 = 0; a3 < a2.size() && a3 < a1; a3++) {
             int a6 = a2.get(a3);
             int a10 = a6 - 1;
-            if (a6 >= 0 && (a10) < 20) {
+            if (
+                    a6 >= 0
+                            && (a10) < 20) {
                 a9.append(a6).append(" ");
             } else {
                 a5++;
             }
         }
         a9.append("\n");
-
+        extracted2(a9);
         return new Result(a5, a9.toString());
     }
+
+    private static void extracted2(StringBuilder a9) {
+        if (a9.length() >= 10) { a9.append("liste trop longue");}
+    }
+
 }
