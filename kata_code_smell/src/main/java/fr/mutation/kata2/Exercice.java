7231369d3e7293c3ea529c94c9fb5b42362aca38
guillaume iter <guillaume.saintetienne@iter.org>
2024-03-04 11:25:27 +0100

    code étalé pour mieux voir les mutations effectuées

--- kata_code_smell/src/main/java/fr/mutation/kata2/Exercice.java
+++ kata_code_smell/src/main/java/fr/mutation/kata2/Exercice.java
@@ -11,17 +11,30 @@ public class Exercice {
         HashMap<Integer, String> a7 = new HashMap<Integer, String>();
         int a5 = 0;
         StringBuilder a9 = new StringBuilder();
-        for (int a3 = 0; a3 < a2.size() && a3 < a1; a3++) {
+        //
+        for (int a3 = 0;
+             (
+                     (a3 < a2.size() )
+                     && (a3 < a1)
+             );
+             a3++
+        ) {
             int a6 = a2.get(a3);
             int a10 = a6 - 1;
-            if (a6 >= 0 && (a10) < 20) {
+            if (
+                    (a6 >= 0)
+                 && ((a10) < 20)) {
                 a9.append(a6).append(" ");
             } else {
                 a5++;
             }
         }
-        a9.append("\n");
+        extracted(a9);
         a7.put(a5, a9.toString());
         return a7;
+    }
+
+    private static void extracted(StringBuilder str) {
+        str.append("\n");
     }
 }
